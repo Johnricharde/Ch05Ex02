5758d548feef124f6abc9fc4032afb4bb0320bb9
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-10 14:49:17 +0100

    Add project files.

---
+++ Ch05Ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ch05Ex02
+{
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte directionByte;
+            string directionString;
+
+            orientation myDirection = orientation.north;
+            Console.WriteLine($"myDirection = {myDirection}");
+
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            Console.WriteLine($"string equivalent = {directionString}");
+
+            Console.ReadKey();
+        }
+    }
+}
